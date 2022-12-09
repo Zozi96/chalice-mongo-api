47b15b924fdc56ce7490cb8e1938630a888c5d72
zozi <zozi.fer96@gmail.com>
2022-12-09 13:33:29 -0600

    Interactive shell for changing environment variables

--- code/deploy.py
+++ code/deploy.py
@@ -45,8 +45,34 @@ class Deploy:
 
 questions: tuple = (
     inquirer.List(name='stage', message='What stage you want to deploy?', choices=('dev', 'prod')),
+    inquirer.Confirm(name='envs', message='Do you want to modify environment variables?', default=False)
 )
+
+
+@dataclass
+class ChangeEnvs:
+    envs: bool = False
+
+    @staticmethod
+    def __update_config():
+        for key, value in environments.items():
+            try:
+                question = (inquirer.Confirm(name=key, message=f'Do you want to change {key}?', default=False),)
+                if inquirer.prompt(question)[key]:
+                    question = (inquirer.Text(name=key, message=f'New value for {key}', default=value),)
+                    environments[key] = inquirer.prompt(question)[key]
+            except KeyError:
+                print('Cancelled by user')
+                exit(1)
+
+    def __call__(self, *args, **kwargs):
+        if self.envs:
+            self.__update_config()
+
 
 if __name__ == '__main__':
     answers: dict = inquirer.prompt(questions)
+    if not answers:
+        exit(1)
+    ChangeEnvs(envs=answers['envs'])()
     Deploy(stage=answers.get('stage'))()
